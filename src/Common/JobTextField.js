b284b7ccd8cbd717e52fd7a5a98d3f2f45fb138f
Daxesh Chauhan <daxeshchauhan1615@gmail.com>
2025-04-05 09:28:15 +0530

    login page validation

--- src/Common/JobTextField.js
+++ src/Common/JobTextField.js
@@ -6,6 +6,9 @@ const JobTextField = ({ label, value, name, onChange, type = "text",
     required = false,
     fullWidth = true, variant = 'outlined',
     size = 'medium',
+    error,
+    helperText,
+    onBlur,
     sx = {},
 }) => {
     return (
@@ -13,12 +16,15 @@ const JobTextField = ({ label, value, name, onChange, type = "text",
             name={name}
             value={value}
             onChange={onChange}
+            onBlur={onBlur}
             type={type}
             placeholder={placeholder}
             required={required}
             fullWidth={fullWidth}
             variant={variant}
             size={size}
+            error={error}
+            helperText={helperText}
             sx={{ my: 1.5, ...sx }} />
 
     )
--- src/app/authentication/login/page.js
+++ src/app/authentication/login/page.js
@@ -1,11 +1,37 @@
 "use client"
+import React from 'react'
 import JobTextField from '@/Common/JobTextField'
 import { Box, Button, Grid, Paper, Typography } from '@mui/material'
-import React from 'react'
 import { useRouter } from "next/navigation"
+import { useFormik } from "formik"
+import * as Yup from "yup"
+
+export const loginSchema = Yup.object().shape({
+    email: Yup.string()
+        .email('Invalid email address')
+        .required('Email is required'),
+
+    password: Yup.string()
+        .min(8, 'Password must be at least 8 characters')
+        .matches(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/,
+            'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character'
+        )
+        .required('Password is required'),
+});
 
 const Login = () => {
     const router = useRouter()
+    const { initialValues, errors, values, handleChange, handleSubmit, handleBlur, handleReset, touched } = useFormik({
+        initialValues: {
+            email: "",
+            password: ""
+        },
+        validationSchema: loginSchema,
+        onSubmit: (value) => {
+            console.log("VALUES", values)
+        }
+    })
     return (
         <Grid
             container
@@ -19,31 +45,43 @@ const Login = () => {
         >
             <Paper elevation={3} sx={{ padding: 4, width: 350, borderRadius: 3 }}>
                 <Box display="flex" flexDirection="column" gap={2}>
-                    <Typography variant="h5" textAlign="center">
-                        Login
-                    </Typography>
-                    <JobTextField
-                        label="Email"
-                        name="email"
-                        onChange={(e) => console.log("Email:", e.target.value)}
-                    />
-                    <JobTextField
-                        label="Password"
-                        name="password"
-                        type="password"
-                        onChange={(e) => console.log("Password:", e.target.value)}
-                    />
-                    <Button variant="contained" fullWidth>
-                        Login
-                    </Button>
-                    <Typography
-                        textAlign="center"
-                        color="primary"
-                        onClick={() => router.push("/authentication/register")}
-                        sx={{ cursor: "pointer" }}
-                    >
-                        Create an account
-                    </Typography>
+                    <form onSubmit={handleSubmit}>
+                        <Typography variant="h5" textAlign="center">
+                            Login
+                        </Typography>
+                        <JobTextField
+                            label="Email"
+                            name="email"
+                            value={values.email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            size='small'
+                            error={Boolean(errors.email && touched.email)}
+                            helperText={errors.email && touched.email ? errors.email : ""}
+                        />
+                        <JobTextField
+                            label="Password"
+                            name="password"
+                            type="password"
+                            size='small'
+                            value={values.password}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            error={Boolean(errors.password && touched.password)}
+                            helperText={errors.password && touched.password ? errors.password : ""}
+                        />
+                        <Button variant="contained" fullWidth type='submit' >
+                            Login
+                        </Button>
+                        <Typography
+                            textAlign="right"
+                            color="primary"
+                            onClick={() => router.push("/authentication/register")}
+                            sx={{ cursor: "pointer", my: 1 }}
+                        >
+                            Create an account
+                        </Typography>
+                    </form>
                 </Box>
             </Paper>
         </Grid>

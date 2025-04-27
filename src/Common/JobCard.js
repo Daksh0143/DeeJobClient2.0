51195a8fa9b5762b7ddfa03f691fb3fc512fe883
Daxesh Chauhan <daxeshchauhan1615@gmail.com>
2025-04-27 18:09:43 +0530

    card changes

--- src/Common/JobCard.js
+++ src/Common/JobCard.js
@@ -4,15 +4,37 @@ import CardHeader from '@mui/material/CardHeader';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
-const JobCard = () => {
+const JobCard = ({
+    title,
+    description,
+    createDate,
+    category,
+    city,
+    location,
+    salaryFrom,
+    salaryTo,
+    fixedSalary,
+
+
+}) => {
+
+    const renderSalary = () => {
+        if (fixedSalary) {
+            return `₹ ${fixedSalary}`;
+        } else if (salaryFrom && salaryTo) {
+            return `₹ ${salaryFrom} - ₹ ${salaryTo}`;
+        } else {
+            return 'Not Mentioned';
+        }
+    };
 
     return (
 
         <Card sx={{ maxWidth: 345, ml: 1, boxShadow: 3, borderRadius: 2 }}>
             <CardHeader
                 sx={{ bgcolor: "#1976D2" }}
-                title="Hiring juniour Developer" // TITLE
-                subheader="September 14, 2016"  // createdBy
+                title={title || "Hiring juniour Developer"}  // TITLE
+                subheader={createDate || "September 14, 2016"}  // createdBy
                 slotProps={{
                     title: {
                         sx: {
@@ -28,17 +50,20 @@ const JobCard = () => {
             />
 
 
-            <CardContent disableSpacing>
+            <CardContent>
                 <Typography >
                     {/* Description */}
                     <span style={{ fontWeight: 600 }}>Description : </span>
-                    Our Company is hiring 0.6-1 year experience developer
+                    {description || "Our Company is hiring 0.6-1 year experience developer"}
                 </Typography>
-                <Typography><span style={{ fontWeight: 600 }}>Category :</span> Jr.Softare Engineer</Typography>
+                <Typography><span style={{ fontWeight: 600 }}>Category :</span> {category || "Jr.Softare Engineer"}</Typography>
 
-                <Typography> <span style={{ fontWeight: 600 }}>Location : </span>  Ahmedabad Navrangpura</Typography>
-                <Typography> <span style={{ fontWeight: 600 }}>Salary  : </span> 20000-40000 </Typography>
+                <Typography> <span style={{ fontWeight: 600 }}>Location : </span> {city || "Ahmedabad"} {location || "Navrangpura"}</Typography>
 
+                <Typography>
+                    <span style={{ fontWeight: 600 }}>Salary: </span>
+                    {renderSalary()}
+                </Typography>
 
 
             </CardContent>
